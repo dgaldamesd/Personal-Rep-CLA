84ef745a755a8e126d5f1b8b6cbf7f360375be8d
d.galdamesd-CLA <d.galdamesd@gmail.com>
2023-11-15 09:29:15 -0300

    Actualizacion de diseño web

--- grafico.py
+++ grafico.py
@@ -1,44 +1,50 @@
 from flask import Flask, render_template, request
-import subprocess
 import sqlite3
-
-import matplotlib.pyplot as plt
-import io
+import pandas as pd  # Agrega esta línea
+import plotly.express as px
+import plotly.io as pio
 import base64
 
 
 app = Flask(__name__)
 
-@app.route('/')
-def mostrar_llamadas():
+def obtener_datos(tabla):
     conn = sqlite3.connect('llamadas.db')
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM llamadas ORDER BY id DESC")  # Seleccionar solo la columna 'contestada'
-    llamadas = cursor.fetchall()
+
+    cursor.execute(f"SELECT Respuesta, COUNT(*) FROM {tabla} GROUP BY Respuesta")
+    resultados = cursor.fetchall()
+
     conn.close()
 
-    print(llamadas)
+    return resultados
 
-    # Procesamiento de los datos para el gráfico
-    llamadas_contestadas = sum(1 for llamada in llamadas if "Call answered and ended by the user" in llamada[0])
-    llamadas_no_contestadas = sum(1 for llamada in llamadas if "Call Rejected by user" in llamada[0])
+def generar_grafico(datos):
+    df = pd.DataFrame(datos, columns=['Respuesta', 'Cantidad'])
 
-    # Crear el gráfico
-    labels = ['Contestadas', 'No Contestadas']
-    cantidad_llamadas = [llamadas_contestadas, llamadas_no_contestadas]
+    fig = px.bar(df, x='Respuesta', y='Cantidad', color='Respuesta',
+                 labels={'Respuesta': 'Respuesta de llamadas', 'Cantidad': 'Cantidad'},
+                 title='Estadísticas de llamadas')
+    
+    return pio.to_html(fig, full_html=False)
 
-    plt.figure(figsize=(6, 4))
-    plt.bar(labels, cantidad_llamadas, color=['green', 'red'])
-    plt.title('Cantidad de llamadas contestadas y no contestadas')
+@app.route('/', methods=['GET', 'POST'])
+def mostrar_grafico():
+    tabla_seleccionada = 'llamadas_november'
 
-    # Convertir el gráfico a una imagen base64
-    img = io.BytesIO()
-    plt.savefig(img, format='png')
-    img.seek(0)
-    grafico_base64 = base64.b64encode(img.getvalue()).decode()
+    if request.method == 'POST':
+        tabla_seleccionada = request.form['tabla']
 
-    return render_template('alertas.html', llamadas=llamadas, grafico_base64=grafico_base64)
+    datos = obtener_datos(tabla_seleccionada)
+    grafico_html = generar_grafico(datos)
+
+    conn = sqlite3.connect('llamadas.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tablas_disponibles = [row[0] for row in cursor.fetchall()]
+    conn.close()
+
+    return render_template('grafico.html', tabla_seleccionada=tabla_seleccionada, grafico_html=grafico_html, tablas_disponibles=tablas_disponibles)
 
 if __name__ == '__main__':
-    with app.app_context():
-        app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(debug=True)
--- webapp2.py
+++ webapp2.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, request
 import subprocess
 import sqlite3
+import pandas as pd  # Agrega esta línea
+import plotly.express as px
+import plotly.io as pio
+import base64
 
 app = Flask(__name__)
 
@@ -17,6 +21,46 @@ def LOGIN():
 @app.route('/INICIO')
 def INICIO():
     return render_template('inicio.html')
+
+# ---------------------------------------PAGINA ESTADISTICAS-----------------------------------------#
+def obtener_datos(tabla):
+    conn = sqlite3.connect('llamadas.db')
+    cursor = conn.cursor()
+
+    cursor.execute(f"SELECT Respuesta, COUNT(*) FROM {tabla} GROUP BY Respuesta")
+    resultados = cursor.fetchall()
+
+    conn.close()
+
+    return resultados
+
+def generar_grafico(datos):
+    df = pd.DataFrame(datos, columns=['Respuesta', 'Cantidad'])
+
+    fig = px.bar(df, x='Respuesta', y='Cantidad', color='Respuesta',
+                 labels={'Respuesta': 'Respuesta de llamadas', 'Cantidad': 'Cantidad'},
+                 title='Estadísticas de llamadas')
+    
+    return pio.to_html(fig, full_html=False)
+
+@app.route('/ESTADISTICAS', methods=['GET', 'POST'])
+def mostrar_grafico():
+    tabla_seleccionada = 'llamadas_november'
+
+    if request.method == 'POST':
+        tabla_seleccionada = request.form['tabla']
+
+    datos = obtener_datos(tabla_seleccionada)
+    grafico_html = generar_grafico(datos)
+
+    conn = sqlite3.connect('llamadas.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tablas_disponibles = [row[0] for row in cursor.fetchall()]
+    conn.close()
+
+    return render_template('grafico.html', tabla_seleccionada=tabla_seleccionada, grafico_html=grafico_html, tablas_disponibles=tablas_disponibles)
+
 
 # ---------------------------------------PAGINA ALERTAS-----------------------------------------#
 @app.route('/ALERTAS', methods=['GET', 'POST'])
